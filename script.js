c5050cf3d61b05771b0709d94613ad866c02712a
Nathan Le Biez <nthnlbz@gmail.com>
2025-09-27 10:31:44 +0200

    compteur du score fonctionnel

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const canvas = document.getElementById("pong");
 const context = canvas.getContext("2d");
 const buttons = document.querySelectorAll(".button");
-
+const startButton = document.getElementById("startButton");
+let win = true;
+let move = false;
+let eventKey = null
+const time = document.getElementById("time");
+let startTime = null;
 
 
 const ball = {
@@ -23,6 +28,19 @@ const pad = {
   color: "BLUE",
 };
 
+function reset(){
+    ball.x = canvas.width / 2;  
+    ball.y = canvas.height - 30;
+    ball.vx = 0.55;
+    ball.vy = -1;
+    pad.x = (canvas.width - 75) / 2;
+    win = true;
+    move = false;
+    eventKey = null;
+    time.textContent = "0";
+    startButton.disabled = false;
+    alert("resetOK");
+}
 
 
 function drawBall() {
@@ -41,6 +59,7 @@ function drawPad() {
     context.closePath();
 }
 
+
 function moveBall() {
     ball.x += ball.vx * ball.speed;
     ball.y += ball.vy * ball.speed;
@@ -50,35 +69,85 @@ function moveBall() {
     if (ball.y - ball.radius < 0) {
         ball.vy = -ball.vy;
     }
+    if(ball.x > pad.x && ball.x < pad.x + pad.width && ball.y + ball.radius === pad.y){
+        ball.vy = -ball.vy;
+    }
+    if (ball.y > canvas.height) {
+       win = false
+    }
+}
+
+function updateTime(){
+    let timeStart = Math.floor((Date.now() - startTime) / 1000);
+    time.textContent = timeStart;
+
 }
 
 
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (button.id === "left" && pad.x >= 0) { 
-        pad.x -= pad.speed;
-    } else if (button.id === "right" && pad.x <= canvas.width - pad.width) {
-        pad.x += pad.speed;
+  button.addEventListener("touchstart", () => {
+    if (button.id === "left" || button.id === "right"){
+      move = true;
+      eventKey = button.id;
     }
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-  })});
-
-
-  document.addEventListener("keydown", (event) => {
-    if (event.key === "ArrowLeft" && pad.x >= 0) { 
-        pad.x -= pad.speed;
-    } else if (event.key === "ArrowRight" && pad.x <= canvas.width - pad.width) {
-        pad.x += pad.speed;
-    }
-    context.clearRect(0, 0, canvas.width, canvas.height);
   });
 
-function loop(){
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawPad();
-    drawBall();
-    requestAnimationFrame(loop);
+  button.addEventListener("touchend", () => {
+    move = false;
+    eventKey = null;
+  });
+});
+
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowLeft" || event.key === "ArrowRight") { 
+        move = true;
+        eventKey = event.key;
+    }
+});
+
+document.addEventListener("keyup", (event) => {
+    if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
+        move = false;
+        eventKey = null;
+    }
+});
+
+function movePad() {
+    if (move) {
+        if ((eventKey === "ArrowLeft" ||  eventKey === "left") && pad.x > 0) {
+            pad.x -= pad.speed;
+        } else if ((eventKey === "ArrowRight" || eventKey === "right") && pad.x < canvas.width - pad.width) {
+            pad.x += pad.speed;
+        }
+    }
 }
 
-loop();
+function draw(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawPad();
+    drawBall(); 
+}
+
+function loop(){
+    if (!win) {
+        alert("Game Over");
+        reset();
+        draw();
+        return;
+    }
+    updateTime();
+    draw();
+    movePad();
+    moveBall();
+    requestAnimationFrame(loop);
+};
+
+
+
+draw();
+startButton.addEventListener("click", () => {
+    startTime = Date.now();
+    startButton.disabled = true;
+    loop();   
+});
