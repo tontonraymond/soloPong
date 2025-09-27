8e31b80ccd38d97e5977ba8b9f9959bf6a3b7b7f
Nathan Le Biez <nthnlbz@gmail.com>
2025-09-27 16:45:06 +0200

    bouton "arreter le jeu" fonctionnel et vitesse augmente + réglages de bugs

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ let move = false;
 let eventKey = null
 const time = document.getElementById("time");
 let startTime = null;
+let gameOn = false;
 
 
 const ball = {
   x: canvas.width / 2,
   y: canvas.height - 30,
-  vx: 0.55,
+  vx: Math.random() * (1 - (- 1) ) - 1,
   vy: -1,
   radius: 10,
   speed: 3,
@@ -31,15 +32,16 @@ const pad = {
 function reset(){
     ball.x = canvas.width / 2;  
     ball.y = canvas.height - 30;
-    ball.vx = 0.55;
+    ball.vx = Math.random() * (1 - (- 1) ) - 1;
     ball.vy = -1;
     pad.x = (canvas.width - 75) / 2;
     win = true;
     move = false;
     eventKey = null;
     time.textContent = "0";
-    startButton.disabled = false;
-    alert("resetOK");
+    startButton.textContent = "Demarrer le jeu";
+    gameOn = false;
+    ball.speed = 3;
 }
 
 
@@ -65,11 +67,21 @@ function moveBall() {
     ball.y += ball.vy * ball.speed;
     if (ball.x + ball.radius > canvas.width || ball.x - ball.radius  < 0) {
         ball.vx = -ball.vx;
+        if (ball.speed < 15){
+            ball.speed += 0.5;
+        }
+        
     }
     if (ball.y - ball.radius < 0) {
+        if (ball.speed < 15){
+            ball.speed += 0.5;
+        }
         ball.vy = -ball.vy;
     }
-    if(ball.x > pad.x && ball.x < pad.x + pad.width && ball.y + ball.radius === pad.y){
+    if(ball.x > pad.x && ball.x < pad.x + pad.width && ball.y + ball.radius > pad.y){
+        if (ball.speed < 15){
+            ball.speed += 0.5;
+        }
         ball.vy = -ball.vy;
     }
     if (ball.y > canvas.height) {
@@ -130,8 +142,10 @@ function draw(){
 }
 
 function loop(){
-    if (!win) {
-        alert("Game Over");
+    if (!win || !gameOn) {
+        if (!win) {
+            alert("Game Over");
+        }
         reset();
         draw();
         return;
@@ -147,7 +161,15 @@ function loop(){
 
 draw();
 startButton.addEventListener("click", () => {
-    startTime = Date.now();
-    startButton.disabled = true;
-    loop();   
+    if (!gameOn) {
+        startTime = Date.now();
+        startButton.textContent = "Arreter le jeu";
+        gameOn = true;
+        loop();
+    }else{
+        startButton.textContent = "Demarrer le jeu";
+        reset();
+        draw();
+        gameOn = false;
+    }
 });
